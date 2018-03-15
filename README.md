4f7d87666c2cebd62ba8fac2bf0cf08743ff1d39
xiaobao <mailbaoqiang@gmail.com>
2018-03-15 15:33:34 +0800

    xiao spring boot and java messy code init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+messy java code with spring boot
---
+++ src/main/java/org/xiao/springboot/XiaoApplication.java
@@ -0,0 +1,12 @@
+package org.xiao.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoApplication.class, args);
+	}
+}
